0b5ace073585d1adba1ef8a1c786ea31df5181be
malmax <malmax.spb@gmail.com>
2016-10-23 00:49:59 +0300

    third task completed

--- Lesson02HW/3task.js
+++ Lesson02HW/3task.js
@@ -6,17 +6,25 @@ console.log все числа от 1 до 100, с двумя исключени�
 «FizzBuzz» для всех чисел, которые делятся и на 3 и на 5.*/
 
 "use strict";
-var start = 0;
+var start = 1;
 var end = 100;
 var output = "";
+var output2 = "";
 
-for (var current = start; current <= end; output= ++current) {
-  if(current % 3 == 0 && current % 5 != 0)
-  {
-    output+=" Fizz";
-  }
-  else if (current % 5 == 0 && current % 3 != 0) {
-    output += " Buzz";
-  }
-    console.log(output);
+for (var current = start; current <= end; output2 = "", current++) {
+
+    output = current;
+
+    if (current % 3 === 0 && current % 5 !== 0) //делится без остатка на 3, но не на 5
+    {
+        output += " Fizz";
+    } else if (current % 5 === 0 && current % 3 !== 0) { //делится без остатка на 5, но не на 3
+        output += " Buzz";
+    }
+
+    if (current % 3 === 0 && current % 5 === 0) { //делится и на 3 и на 5
+        output2 += " FizzBuzz";
+    }
+
+    console.log(output + "" + output2);
 }
