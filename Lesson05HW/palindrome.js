ceaf67cc5bebc80d2fd4697e9ac751605bb1bcaa
malmax <malmax.spb@gmail.com>
2016-11-07 18:31:58 +0300

    complete lesson06hw

--- Lesson05HW/palindrome.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(s) {
-    s=s.toLowerCase().match(/(\w|\d)/g);
-    return s.slice(0).reverse()+""==s?true:false;
-}
-console.log(isPalindrome("level"));                   // logs 'true'
-console.log(isPalindrome("levels"));                  // logs 'false'
-console.log(isPalindrome("A car, a man, a maraca"));  // logs 'true'
