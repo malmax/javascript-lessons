e6e3c1193d046436cac6e4bb3e4fb35b82870409
malmax <malmax.spb@gmail.com>
2016-11-07 17:00:43 +0300

    palindrome

--- Lesson05HW/palindrome.js
+++ Lesson05HW/palindrome.js
@@ -1,10 +1,6 @@
 function isPalindrome(s) {
-    s = s.match(/(\w|\d)/gi);
-    n = s.slice(0).reverse()+"";
-    console.log(n);
-    console.log(s+"");
-    return (""+s).search(/n/i);
-    // return (ns == s ? true : false);
+    s=s.toLowerCase().match(/(\w|\d)/g);
+    return s.slice(0).reverse()+""==s?true:false;
 }
 console.log(isPalindrome("level"));                   // logs 'true'
 console.log(isPalindrome("levels"));                  // logs 'false'
