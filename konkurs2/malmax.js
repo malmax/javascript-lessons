88bb59010bebcb5a0a1d2a67246ba445ff7ce12a
malmax <malmax.spb@gmail.com>
2016-12-14 17:29:03 +0300

    complete

--- konkurs2/malmax.js
+++ konkurs2/malmax.js
@@ -1,12 +1,12 @@
 "use strict";
 
 function check(str) {    
-    var a = str.match(/[\(\{\[\]\}\)]/g);    
+    var a = str.match(/[\(\{\[\]\}\)]/g); // оставляем только скобки и приводим к массиву   
     var total;
     do {
-        var b = a.shift().charCodeAt();
-        b = b == 40 ? b + 1 : b + 2; //( - 40 ) - 41  
-        total = b == a.pop().charCodeAt();      
+        var b = a.shift().charCodeAt(); // первый элемент массива к коду
+        b = b == 40 ? b + 1 : b + 2; //( - 40 ) - 41, а у других скобок разница в 2 
+        total = b == a.pop().charCodeAt(); // сравниваем первый и последний элемент массива     
     } while(total && a.length > 1)
     return !a.length && total;
 }
