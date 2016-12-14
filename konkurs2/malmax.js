d73728e56611e720d390dce50e3ff2b6b826d1bb
malmax <malmax.spb@gmail.com>
2016-12-14 17:27:00 +0300

    complete

--- konkurs2/malmax.js
+++ konkurs2/malmax.js
@@ -2,15 +2,17 @@
 
 function check(str) {    
     var a = str.match(/[\(\{\[\]\}\)]/g);    
+    var total;
     do {
         var b = a.shift().charCodeAt();
-        b = b == 40 ? b + 1 : b + 2; //( - 40 ) - 41        
-    } while(b == a.pop().charCodeAt() && a.length > 1)
-    return !a.length;
+        b = b == 40 ? b + 1 : b + 2; //( - 40 ) - 41  
+        total = b == a.pop().charCodeAt();      
+    } while(total && a.length > 1)
+    return !a.length && total;
 }
 
-['({})', '[]', '[{()}]', ' {(})', '([]', '[])','(])'].forEach(function(test) {
-    console.log(test + ' - ' + check(test));
+['t({e}s)t', 'te[st]', '[t{e(s)t}]', '{t(e}s)t','t {e(}s}t', 't(e[st]', '[])test','(tes]t)'].forEach(function(test) {
+    console.log(test.replace(/[^\(\{\[\]\}\)]/g,'') + ' - ' + check(test));
 });
 
 
